55679f07d2a36ef7e2637047d07c191590fc9f16
Duong Dat <tao@cu8-3.local>
2019-12-31 04:31:54 +0700

    ok

---
+++ precache-manifest.df5db4fe12ef779dee8c82be2f1bdf7c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ed2dd78dcc87a11ab97d628e2dc2a0d6",
+    "url": "/image-puzzle/index.html"
+  },
+  {
+    "revision": "2cedeaef5f0f26a37239",
+    "url": "/image-puzzle/static/css/main.01d54325.chunk.css"
+  },
+  {
+    "revision": "a81c7f64c68880b0247f",
+    "url": "/image-puzzle/static/js/2.7a975201.chunk.js"
+  },
+  {
+    "revision": "d9b445fd12d8bcaca03d304ff543ec4e",
+    "url": "/image-puzzle/static/js/2.7a975201.chunk.js.LICENSE"
+  },
+  {
+    "revision": "2cedeaef5f0f26a37239",
+    "url": "/image-puzzle/static/js/main.82cbcd4e.chunk.js"
+  },
+  {
+    "revision": "aede64837a5aaa9896ec",
+    "url": "/image-puzzle/static/js/runtime-main.3ce6bb7f.js"
+  }
+]);
